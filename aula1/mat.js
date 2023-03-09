229e356969134e11dc60e5d1b07c9159e3824378
Eduardo Andrade <eduardoandraade@outlook.com>
2023-03-09 09:04:19 -0300

    subindo aulas já feitas

---
+++ aula1/mat.js
@@ -0,0 +1,8 @@
+var num = 10
+var num1 = 10
+
+function somar(a,b) {
+    return a + b;
+}
+
+console.log(somar(num, num1))
---
+++ aula1/nome.js
@@ -0,0 +1,4 @@
+var nome = "Eduardo"
+var sobrenome = "Andrade"
+
+console.log(nome + " " + sobrenome);
